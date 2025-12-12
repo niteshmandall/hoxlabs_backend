9525f10097dc2138e38cdb8aa9c73c5607a1bf04
niteshmandal0 <niteshnunfara0@gmail.com>
2025-12-13 00:03:26 +0530

    Initial commit

---
+++ src/test/java/com/hoxlabs/HoxlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoxlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoxlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

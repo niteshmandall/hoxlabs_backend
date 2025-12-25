ea39c7c23b3b414283bcab1328091b0f8adec10a
niteshmandall <niteshnunfara0@gmail.com>
2025-12-25 18:54:46 +0530

    added junit testing

---
+++ src/test/java/com/hoxlabs/calorieai/CalorieAiTestSuite.java
@@ -0,0 +1,9 @@
+package com.hoxlabs.calorieai;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.hoxlabs.calorieai")
+public class CalorieAiTestSuite {
+}
